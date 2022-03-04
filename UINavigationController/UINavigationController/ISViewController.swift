24fe53e0c1b725d25dfe5353782ebda770cf390b
mkatariya19 <100358643+mkatariya19@users.noreply.github.com>
2022-03-04 19:43:48 +0530

    Add files via upload

---
+++ UINavigationController/UINavigationController/ISViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ISViewController.swift
+//  UINavigationController
+//
+//  Created by Coditas on 04/03/22.
+//
+
+import UIKit
+
+class ISViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Is Screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
